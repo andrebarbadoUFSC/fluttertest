ef6f9f93fb3fe93e1a91ce7af0502ed810299f28
André Luís Corrêa Barbado <andre.barbado@grad.ufsc.br>
2022-08-17 00:06:44 -0300

    botao central

--- lib/main.dart
+++ lib/main.dart
@@ -5,31 +5,33 @@ void main() {
 }
 
 class MyApp2 extends StatelessWidget {
-  const MyApp2({Key? key}) :super(key: key);
+  const MyApp2({Key? key}) : super(key: key);
 
-  @override 
-  Widget build(BuildContext context) {                      //tira o debug banner
+  @override
+  Widget build(BuildContext context) {
+    //tira o debug banner
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(                                       // scaffold eh a parte principal do app a pag principal
-        appBar: AppBar(                                     // criei barra de cima 
+      home: Scaffold(
+        // scaffold eh a parte principal do app a pag principal
+        appBar: AppBar(
+          // criei barra de cima
           title: const Text('FigurIt'),
-        ),                                                 
-        body: const Text('body'),
+        ),
+        body: Center(
+          child: ElevatedButton(onPressed: (){}, child: Text('Click')),
+        ), // mandandi para o meio, botao direito -> refactor
 
         bottomNavigationBar: BottomNavigationBar(
           items: const [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(Icons.home)
-              ),
+            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
             BottomNavigationBarItem(
               label: 'Settings',
               icon: Icon(Icons.settings),
-              )
+            )
           ],
         ),
       ),
     );
-  } 
+  }
 }
