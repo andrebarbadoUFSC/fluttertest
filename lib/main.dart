5b9bd1174f134cb8a8d22175b5682ac22fc533f4
André Luís Corrêa Barbado <andre.barbado@grad.ufsc.br>
2022-08-17 03:49:02 -0300

    inserindo imagem local

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _MyApp2State extends State<MyApp2> {
           child: indexAtual == 0 ? Container(
             width: double.infinity,
             height: double.infinity,
-            color: Color.fromARGB(255, 26, 136, 16),
+            color: const Color.fromARGB(255, 26, 136, 16),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -62,8 +62,9 @@ class _MyApp2State extends State<MyApp2> {
               ],
             ),
           )
-        : Image.network(
-          'https://s2.glbimg.com/XyUa9fSiGGOOz269tQqVJhs7rL4=/0x0:550x638/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_f035dd6fd91c438fa04ab718d608bbaa/internal_photos/bs/2022/F/O/pQRBBKStecsmgbgEh0dA/capa-album-copa.jpg'),
+        : Image.asset(
+          'images/imagetest.jpg'
+          ),
         ), // mandandi para o meio, botao direito -> refactor
 
         bottomNavigationBar: BottomNavigationBar(
