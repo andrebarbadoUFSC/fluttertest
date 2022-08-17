6b2b1172ad4e7031574f62d61f29af25c7c88ee6
André Luís Corrêa Barbado <andre.barbado@grad.ufsc.br>
2022-08-16 21:28:48 -0300

    upload code

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp2());
+}
+
+class MyApp2 extends StatelessWidget {
+  const MyApp2({Key? key}) :super(key: key);
+
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(                                       // scaffold eh a parte principal do app a pag principal
+        appBar: AppBar(
+          title: Text('FigurIt'),
+        ),                                   // criei barra de cima 
+      ),
+    );
+  } 
+
+}
