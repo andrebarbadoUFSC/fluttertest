daaaaa80a946a8219ca982e25bb82555aa9b49e3
André Luís Corrêa Barbado <andre.barbado@grad.ufsc.br>
2022-08-16 23:59:02 -0300

    Icones add

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,28 @@ class MyApp2 extends StatelessWidget {
   const MyApp2({Key? key}) :super(key: key);
 
   @override 
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {                      //tira o debug banner
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(                                       // scaffold eh a parte principal do app a pag principal
-        appBar: AppBar(
-          title: Text('FigurItaas'),
-        ),                                   // criei barra de cima 
+        appBar: AppBar(                                     // criei barra de cima 
+          title: const Text('FigurIt'),
+        ),                                                 
+        body: const Text('body'),
+
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home)
+              ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),
+              )
+          ],
+        ),
       ),
     );
   } 
-
 }
